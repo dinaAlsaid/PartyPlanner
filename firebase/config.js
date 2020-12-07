a26736993b7f5df25197ecaf5310e9217b786516
bayan-97 <bayanalalem0@gmail.com>
2020-12-07 10:12:49 +0200

    done

--- firebase/config.js
+++ firebase/config.js
@@ -3,14 +3,13 @@ import '@firebase/firestore';
 import '@firebase/storage';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDeSIEJzAYTU8EP_Q4nqX0KAaMDThQxrpY",
-  authDomain: "partyplaner-614f2.firebaseapp.com",
-  projectId: "partyplaner-614f2",
-  storageBucket: "partyplaner-614f2.appspot.com",
-  messagingSenderId: "457687109329",
-  appId: "1:457687109329:web:ac11db290f9cf5a9e83ff0",
-  measurementId: "G-9DDTZD7MDQ"
+	apiKey: 'AIzaSyCZQo9KBHizuluhatFj3LoraJEv6BgZg-I',
+	authDomain: 'partyplanner-8871f.firebaseapp.com',
+	projectId: 'partyplanner-8871f',
+	storageBucket: 'partyplanner-8871f.appspot.com',
+	messagingSenderId: '368255489505',
+	appId: '1:368255489505:web:136873af4d4f51576faea0',
+	measurementId: 'G-B1JQFXSJTZ'
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
